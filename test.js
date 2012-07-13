0990e27075b634ac629c275d0eb89c01c73c824c
Louis T <louist@ltdev.im>
2012-07-13 00:53:15 -0400

    This could be much better. [FIX]

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*
-  Test 2, read example database.
+  Test 1, read example database.
 */
-console.log("Test 1: print from an example database.\n");
+console.log("Test 1: print from an example database.");
 var GistDB = require('./');
 var gdb = new GistDB({id:'9879133ed9dd415d7199'});
 gdb.load();
@@ -12,15 +12,16 @@ gdb.on('loaded',function (data) {
        console.log('Example0 failed.');
     }
     console.log('Current data: '+JSON.stringify(this.content));
+    runTest2();
 });
-
-
 /*
   Test 2, no error handler.
 */
-console.log("\n\nTest 2: No error handler.\n");
-var gdb = new GistDB({user:'USERNAME',pass:'PASSWORD',id:'9879133ed9dd415d7199',timeout:5000});
-gdb.load();
-gdb.on('loaded',function (data) {
-    console.log('Current data: '+JSON.stringify(data));
-});
+function runTest2 () {
+         console.log("\nTest 2: No error handler.\n");
+         var gdb = new GistDB({user:'USERNAME',pass:'PASSWORD',id:'9879133ed9dd415d7199',timeout:5000});
+         gdb.load();
+         gdb.on('loaded',function (data) {
+             console.log('Current data: '+JSON.stringify(data));
+         });
+}
